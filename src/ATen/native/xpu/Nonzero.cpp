7050b130b83884e276e2e6714c1d8e88dd12f01a
Slawomir Siwek <slawomir.siwek@intel.com>
2025-08-21 15:54:06 +0000

    Naive implementation

--- src/ATen/native/xpu/Nonzero.cpp
+++ src/ATen/native/xpu/Nonzero.cpp
@@ -1,4 +1,5 @@
 #include <ATen/core/Tensor.h>
+#include <ATen/ops/full.h>
 #include <ATen/xpu/EmptyTensor.h>
 
 #include <ATen/native/xpu/sycl/NonzeroKernel.h>
@@ -38,5 +39,57 @@ Tensor nonzero_xpu(const Tensor& self) {
   nonzero_out_xpu(self, out);
   return out;
 }
+
+Tensor& nonzero_static_out_xpu(
+    const Tensor& self,
+    int64_t size,
+    int64_t fill_value,
+    Tensor& out) {
+  TORCH_CHECK(
+      self.numel() < std::numeric_limits<int>::max(),
+      "nonzero is not supported for tensors with more than INT_MAX elements, \
+  See https://github.com/pytorch/pytorch/issues/51871");
+  TORCH_CHECK(
+      out.dtype() == at::kLong,
+      "Expected object of scalar type ",
+      at::kLong,
+      " as out, but got ",
+      out.dtype()); 
+  TORCH_CHECK(
+      self.device() == out.device(),
+      "expected self and out to be on the same device, but got out on ",
+      out.device(),
+      " and self on ",
+      self.device());
+  TORCH_CHECK(
+      self.dim() <= MAX_DIMS,
+      "nonzero is not supported for tensor with more than ",
+      MAX_DIMS,
+      " dimensions");
+
+  xpu::nonzero_kernel(self, out);
+
+  // Naive implementation to just trim/expand result from existing nonzero_kernel
+  int64_t num_nonzero = out.size(0); 
+  int64_t out_size = std::max(size, num_nonzero);
+  auto filled = at::full({out_size, self.dim()}, fill_value, out.options());
+  if (num_nonzero > 0) {
+    filled.narrow(0, 0, num_nonzero).copy_(out);
+  }
+  out = filled.narrow(0, 0, size);
+  // Naive implementation to just trim/expand result from existing nonzero_kernel
+
+  return out;
+}
+
+Tensor nonzero_static_xpu(
+    const Tensor& self,
+    int64_t size,
+    int64_t fill_value) {
+  Tensor out = at::detail::empty_xpu({size, self.dim()}, self.options().dtype(kLong));
+  nonzero_static_out_xpu(self, size, fill_value, out);
+  return out;
+}
+
 } // namespace native
 } // namespace at
