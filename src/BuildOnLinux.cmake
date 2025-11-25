2ff61bb1e064d17799bf7f0434ab0a46811504a6
Sławomir Siwek <slawomir.siwek@intel.com>
2025-11-25 08:07:28 +0000

    small improvements

--- src/BuildOnLinux.cmake
+++ src/BuildOnLinux.cmake
@@ -3,7 +3,7 @@
 set(TORCH_XPU_OPS_LIBRARIES)
 set(SYCL_LINK_LIBRARIES_KEYWORD PRIVATE)
 
-macro(setup_common_libraries)
+if(BUILD_SEPARATE_OPS)
   add_library(
     torch_xpu_ops
     STATIC
@@ -13,20 +13,8 @@ macro(setup_common_libraries)
     ${ATen_XPU_GEN_SRCS}
     ${ATen_XPU_XCCL_SRCS})
 
-  if(USE_C10D_XCCL)
-    target_compile_definitions(torch_xpu_ops PRIVATE USE_C10D_XCCL)
-    target_link_libraries(torch_xpu_ops PUBLIC torch::xccl)
-    target_link_libraries(torch_xpu_ops PUBLIC fmt::fmt-header-only)
-  endif()
-
-  if(USE_SYCLTLA)
-    target_compile_definitions(torch_xpu_ops PRIVATE USE_SYCLTLA)
-  endif()
   list(APPEND TORCH_XPU_OPS_LIBRARIES torch_xpu_ops)
-endmacro()
 
-if(BUILD_SEPARATE_OPS)
-  setup_common_libraries()
   foreach(sycl_src ${ATen_XPU_SYCL_SRCS})
     get_filename_component(name ${sycl_src} NAME_WLE REALPATH)
     set(sycl_lib torch-xpu-ops-sycl-${name})
@@ -46,21 +34,18 @@ else()
     STATIC
     CXX_SOURCES  ${ATen_XPU_CPP_SRCS} ${ATen_XPU_MKL_SRCS} ${ATen_XPU_NATIVE_CPP_SRCS} ${ATen_XPU_GEN_SRCS} ${ATen_XPU_XCCL_SRCS}
     SYCL_SOURCES ${ATen_XPU_SYCL_SRCS})
-  if(USE_C10D_XCCL)
-    target_compile_definitions(torch_xpu_ops PRIVATE USE_C10D_XCCL)
-    target_link_libraries(torch_xpu_ops  PUBLIC torch::xccl)
-    target_link_libraries(torch_xpu_ops PUBLIC fmt::fmt-header-only)
-  endif()
-
-  if(USE_SYCLTLA)
-    target_compile_definitions(torch_xpu_ops PRIVATE USE_SYCLTLA)
-  endif()
 
   install(TARGETS torch_xpu_ops DESTINATION "${TORCH_INSTALL_LIB_DIR}")
   list(APPEND TORCH_XPU_OPS_LIBRARIES torch_xpu_ops)
 endif()
 
+if(USE_C10D_XCCL)
+  target_compile_definitions(torch_xpu_ops PRIVATE USE_C10D_XCCL)
+  target_link_libraries(torch_xpu_ops PUBLIC torch::xccl fmt::fmt-header-only)
+endif()
+
 if(USE_SYCLTLA)
+  target_compile_definitions(torch_xpu_ops PRIVATE USE_SYCLTLA)
   set(REPLACE_FLAGS_FOR_SYCLTLA TRUE)
   set_build_flags()
   replace_cmake_build_flags()
@@ -96,12 +81,8 @@ foreach(lib ${TORCH_XPU_OPS_LIBRARIES})
   target_compile_options_if_supported(${lib} "-Wno-deprecated-copy")
   target_compile_options(${lib} PRIVATE ${TORCH_XPU_OPS_FLAGS})
 
-  target_include_directories(${lib} PUBLIC ${TORCH_XPU_OPS_INCLUDE_DIRS})
-  target_include_directories(${lib} PUBLIC ${ATen_XPU_INCLUDE_DIRS})
-  target_include_directories(${lib} PUBLIC ${SYCL_INCLUDE_DIR})
-
-  target_link_libraries(${lib} PUBLIC ${SYCL_LIBRARY})
-  target_link_libraries(${lib} PRIVATE ATEN_XPU_OPS_FILES_GEN_LIB)
+  target_include_directories(${lib} PUBLIC ${TORCH_XPU_OPS_INCLUDE_DIRS} ${ATen_XPU_INCLUDE_DIRS} ${SYCL_INCLUDE_DIR})
+  target_link_libraries(${lib} PUBLIC ${SYCL_LIBRARY} PRIVATE ATEN_XPU_OPS_FILES_GEN_LIB)
 endforeach()
 
 if(USE_ONEMKL_XPU)
